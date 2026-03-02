85da220182555109efa762a5b13c0e9f05e84c94
DTID <nuzhat.tasnim.faiza@gmail.com>
2026-03-02 15:11:47 +0600

    commit

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TicketViewSet
+from .views import TicketViewSet, LogoutView
 
 router = DefaultRouter()
-router.register(r'tickets', TicketViewSet, basename='ticket')
+router.register(r'', TicketViewSet, basename='ticket')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
