338ff5bea677b0afa8fd2572801038dd7fab8f89
DTID <nuzhat.tasnim.faiza@gmail.com>
2026-03-02 13:44:02 +0600

    added CRUD properly

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LogoutView, ticket_list_create
+from .views import LogoutView, ticket_detail, ticket_list_create
 
 urlpatterns = [
     path("", ticket_list_create),
     path("logout/", LogoutView.as_view()),
+    path("tickets/<int:pk>/", ticket_detail)
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -8,7 +8,7 @@ from .models import Ticket
 from .serializers import TicketSerializer
 
 
-@api_view(["GET", "POST"])
+@api_view(["GET", "POST", "PUT", "PATCH", "DELETE"])
 @permission_classes([IsAuthenticated])
 def ticket_list_create(request):
     if request.method == "GET":
@@ -22,6 +22,42 @@ def ticket_list_create(request):
             serializer.save(created_by=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
+@permission_classes([IsAuthenticated])
+def ticket_detail(request, pk):
+    try:
+        ticket = Ticket.objects.get(pk=pk)
+    except Ticket.DoesNotExist:
+        return Response(
+            {"error": "Ticket not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    if request.method == "GET":
+        serializer = TicketSerializer(ticket)
+        return Response(serializer.data)
+
+    if request.method == "PUT":
+        serializer = TicketSerializer(ticket, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "PATCH":
+        serializer = TicketSerializer(ticket, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "DELETE":
+        ticket.delete()
+        return Response(
+            {"message": "Ticket deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT
+        )
     
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
